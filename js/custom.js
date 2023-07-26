4f40f51a4685933d9d75272196a79d959cb906b3
David V. Kimball <davidkimball@outlook.com>
2023-07-26 10:01:36 -0700

    Changed theme will now persist and be stored.

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,51 @@
 $(function() {
 
-// Check if dark mode enabled by system
-if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  document.body.classList.add('dark');
-} 
+  // Get system preference
+  const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+  const systemPrefersDark = darkModeMediaQuery.matches;
+  
+  // Get stored preference
+  const storedPref = localStorage.getItem('theme');
 
-// Toggle theme  
-const toggle = document.getElementById('theme-toggle');
+  // Default to system pref
+  let currentTheme = systemPrefersDark ? 'dark' : 'light';
 
-toggle.addEventListener('click', () => {
-  document.body.classList.toggle('dark');
-});
+  // If stored pref, use that
+  if (storedPref) {
+    currentTheme = storedPref;
+  }
+
+  // Set initial theme
+  setTheme(currentTheme);
+
+  // Listen for system pref changes
+  darkModeMediaQuery.addEventListener('change', e => {
+    if (!storedPref) {
+      // If no stored pref, follow system pref
+      setTheme(e.matches ? 'dark' : 'light'); 
+    } 
+  });
+
+  // Toggle button
+  const toggle = document.getElementById('theme-toggle');
+  
+  toggle.addEventListener('click', () => {
+    // Switch theme
+    currentTheme = currentTheme === 'dark' ? 'light' : 'dark';
     
+    // Store preference
+    localStorage.setItem('theme', currentTheme);
+    
+    // Set theme
+    setTheme(currentTheme);
+  });
+
+  function setTheme(theme) {
+    if (theme === 'dark') {
+      document.body.classList.add('dark');
+    } else {
+      document.body.classList.remove('dark');
+    }
+  }
+
 });

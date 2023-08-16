dbe386faf29899a85bbb4212fd2562c1b0ea6582
David V. Kimball <davidkimball@outlook.com>
2023-08-16 14:30:36 -0700

    Brief test.

--- js/html_loader.js
+++ js/html_loader.js
@@ -8,37 +8,39 @@
 ///
 
 const html_templates = {
-"menu": "templates/menu.html",
-}
 
-///
-// CODE
-///
-// Don't edit below this line unless you know what you're doing!
-///
-
-(function () {
-
-const domParser = new window.DOMParser();
-
-Object.keys(html_templates).map(function (element) {
-  const url = html_templates[element];
-
-  fetch(url).then((response) => response.text())
-    .then(function (html) {
-      let parsedHTML = domParser.parseFromString(html, "text/html");
-      let pageElement = document.getElementById(element);
-
-      if (pageElement.replaceChildren !== undefined) {
-        pageElement.replaceChildren(...parsedHTML.body.childNodes);
-      } else {
-        while (pageElement.firstChild) {
-          pageElement.removeChild(pageElement.firstChild);
+  "header": "/templates/menu.html",
+  
+  };
+  
+  ///
+  // CODE
+  ///
+  // Don't edit below this line unless you know what you're doing!
+  ///
+  
+  (function () {
+  
+  const domParser = new window.DOMParser();
+  
+  Object.keys(html_templates).map(function (element) {
+    const url = html_templates[element];
+  
+    fetch(url).then((response) => response.text())
+      .then(function (html) {
+        let parsedHTML = domParser.parseFromString(html, "text/html");
+        let pageElement = document.getElementById(element);
+  
+        if (pageElement.replaceChildren !== undefined) {
+          pageElement.replaceChildren(...parsedHTML.body.childNodes);
+        } else {
+          while (pageElement.firstChild) {
+            pageElement.removeChild(pageElement.firstChild);
+          }
+          pageElement.append(...parsedHTML.body.childNodes);
         }
-        pageElement.append(...parsedHTML.body.childNodes);
-      }
-      document.head.appendChild(parsedHTML.head);
-    });
-});
-
-}());
+        document.head.appendChild(parsedHTML.head);
+      });
+  });
+  
+  }());
